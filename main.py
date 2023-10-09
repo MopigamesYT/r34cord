98adf553fffa9952930bb08a1cb8e51bb3a38f30
Margot <95306417+MopigamesYT@users.noreply.github.com>
2023-10-09 20:54:24 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -20,10 +20,25 @@ async def ping(ctx):
 
 @bot.command()
 async def post(ctx, query, num_posts=1):
+    if num_posts > 5:
+        await ctx.send("Sorry, the maximum number of posts is 5.")
+        return
     await ctx.send(f'Searching for {query}...')
     result_search = r34Py.search([query], limit=num_posts)
+    links = ""
     for post in result_search:
-        await ctx.send(post.image)
+        links += post.image + "\n"
+    await ctx.send(links)
+    # BEGIN: 2d5f1d7d7d5d
+bot.remove_command('help')
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title="Help", description="List of available commands", color=0x00ff00)
+    embed.add_field(name="ping", value="Returns 'Pong!'", inline=False)
+    embed.add_field(name="post", value="Searches for posts on rule34 with the given query and returns the links to the images. Maximum of 5 posts. Usage example: r//post catgirl 3", inline=False)
+    await ctx.send(embed=embed)
+    
 
 bot.run('MTE1OTk2MjUxNTI5MjI5OTI3Ng.GonXlC.l0QNKBtB_lQ8_Kc1I1lZfc2bC_Y-dPxyVvhTQw')
 
