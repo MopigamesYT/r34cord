b7c80157c8687d02fcaa028c9f2c1924a7b7a154
Margot <95306417+MopigamesYT@users.noreply.github.com>
2023-10-11 13:44:17 +0000

    filter debug

--- main.py
+++ main.py
@@ -44,9 +44,11 @@ async def post(ctx, query, num_posts=1):
     links = ""
     for post in result_search:
         if "scat" in post.tags or "bestiality" in post.tags:
-            links += "filtered\n" + post.tags + "\n"
+            links += "filtered\n"
+            print(post.tags)
         else:
-            links += post.image + "\n" + post.tags + "\n"
+            links += post.image + "\n"
+            print(post.tags)
     await ctx.send(links)
 bot.remove_command('help')
 
